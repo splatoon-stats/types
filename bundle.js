0f30fb617e3e3a367aad8d9f22d06a9c20eb8d0f
yukidaruma <yukidaruma@users.noreply.github.com>
2020-11-23 07:27:17 +0900

    chore: Bundle type definitions

---
+++ bundle.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const packageInfo = require('./package.json');
+
+const exclude = ['index'];
+const files = fs.readdirSync('src', { withFileTypes: true });
+
+const bundled =
+    files
+        .filter(file => file.name.endsWith('.ts'))
+        .map(file => file.name.replace(/(?:\.d)?\.ts/, ''))
+        .filter(filename => !exclude.includes(filename))
+        .map(filename => `export * from "./${filename}";`)
+        .join('\n') + '\n';
+
+fs.writeFileSync(packageInfo.types, bundled);
